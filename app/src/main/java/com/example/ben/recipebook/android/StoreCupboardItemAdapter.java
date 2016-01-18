05983083ecb33a9061a4ebe897b2a26f05fb621e
Ben Below <benjamin.below@gmail.com>
2016-01-18 02:32:59 +0000

    Remove items from ingredient list one at a time.

--- app/src/main/java/com/example/ben/recipebook/android/StoreCupboardItemAdapter.java
+++ app/src/main/java/com/example/ben/recipebook/android/StoreCupboardItemAdapter.java
@@ -48,36 +48,10 @@ public class StoreCupboardItemAdapter extends SelectableAdapter<StoreCupboardIte
             }
         });
 
-        // Split the list in ranges
-        while (!positions.isEmpty()) {
-            if (positions.size() == 1) {
-                removeItem(positions.get(0));
-                positions.remove(0);
-            } else {
-                int count = 1;
-                while (positions.size() > count && positions.get(count).equals(positions.get(count - 1) - 1)) {
-                    ++count;
-                }
-
-                if (count == 1) {
-                    removeItem(positions.get(0));
-                } else {
-                    removeRange(positions.get(count - 1), count);
-                }
-
-                for (int i = 0; i < count; ++i) {
-                    positions.remove(0);
-                }
-            }
+        for(int i : positions){
+            removeItem(i);
         }
-    }
 
-    private void removeRange(int positionStart, int itemCount) {
-        for (int i = 0; i < itemCount; ++i) {
-            items.remove(positionStart);
-        }
-        notifyItemRangeRemoved(positionStart, itemCount);
-        notifyItemRangeChanged(positionStart, items.size());
     }
 
     @Override
